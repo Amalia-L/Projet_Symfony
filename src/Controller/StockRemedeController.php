6a2ca245c10c21c1c06436840c2b9222d0edcf2b
Amalia <tit-mali@hotmail.com>
2024-11-21 18:21:01 +0100

    Modif twig

--- src/Controller/StockRemedeController.php
+++ src/Controller/StockRemedeController.php
@@ -2,17 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class StockRemedeController extends AbstractController
 {
-    #[Route('/stock-remede', name: 'app_stock_remede')]
-    public function index(): Response
+    #[Route('/stock-remede', name:"stockRemede.index")]
+    public function index (Request $request): Response
     {
-        return $this->render('stock_remede/index.html.twig', [
-            'controller_name' => 'StockRemedeController',
-        ]);
+        return $this->render('stockRemede/index.html.twig');
     }
 }
